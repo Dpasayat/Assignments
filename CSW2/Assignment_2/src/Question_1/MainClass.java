a4626072849deed513aebf8c282558fa844d7276
Dhiraj <dpasayat14@gmail.com>
2024-03-05 10:01:06 +0530

    Assignment 2 cont

--- CSW2/Assignment_2/src/Question_1/MainClass.java
+++ CSW2/Assignment_2/src/Question_1/MainClass.java
@@ -1,20 +1,29 @@
 package Question_1;
 
+
+
 public class MainClass 
 {
 
 	public static void main(String[] args) 
 	{
-		Student<String> s1=new Student<String>("Dhiraj", "2241016218", 20);
+		Student<Integer> s1=new Student<Integer>("Dhiraj", 6218, 20);
 		System.out.println("Roll Number as a String: ");
 		System.out.println(s1.toString());
-		System.out.println("Type of roll number:");
-		System.out.println(s1.rollNo.getClass());
+		System.out.println("Type of roll number: "+s1.rollNo.getClass());
+		System.out.println();
 		
-		
-		Student<Integer> s2= new Student<Integer>("Dhiraj", 6218, 20);
+		System.out.println("Roll Number as a Integer                                                                                 : ");
+		Student<String> s2= new Student<String>("Dhiraj", "6218", 20);
 		System.out.println(s2.toString());
-		System.out.println(s2.rollNo.getClass());
+		System.out.println("Type of roll number: "+s2.rollNo.getClass());
+		
+//		System.out.println(s1.equals(s2));
+//		System.out.println(s1.hashCode()+"  "+s2.hashCode());
+		
+//		System.out.println(Objects.hash("Aar")+" "+ Objects.hash("Ab"));
+//		System.out.println(s1.compareTo(s2));
+		
 
 	}
 
--- CSW2/Assignment_2/src/Question_1/Student.java
+++ CSW2/Assignment_2/src/Question_1/Student.java
@@ -1,6 +1,13 @@
+//Q1. Write a program to create a Student class that has members, name, roll
+//number, and age. Design the Student class in such a way that it can take
+//the roll number as an integer or string. Create a driver class that creates
+//student objects and invokes the methods.
+
 package Question_1;
 
-public class Student<T> 
+import java.util.Objects;
+
+public class Student<T extends Comparable> implements Comparable<Student<T>>
 {
 	private String name;
 	T rollNo;
@@ -33,9 +40,48 @@ public class Student<T>
 	public String toString() {
 		return "Student [name=" + name + ", rollNo=" + rollNo + ", age=" + age + "]";
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name, rollNo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student<?> other = (Student<?>) obj;
+		return age == other.age && this.name.equals(other.name) && this.rollNo.equals(other.rollNo);
+	}
+	@Override
+	public int compareTo(Student<T> o) 
+	{
+		
+//		if(this.equals(o))
+//		{
+//			return 0;
+//		}
+//		else 
+//		{
+		
+				return this.rollNo.compareTo(o.rollNo);
+//			else
+//				return 1;
+//			
+		}
+	}
+	
 	
 	
 	
 	
 
-}
+
+	//@Override
+//	public int compareTo(Student<T> o) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}}
+//}
