1240898c77f625b6ec0eb1006aa7845782263e4d
Dhiraj <dpasayat14@gmail.com>
2024-03-18 10:27:19 +0530

    Question 11 modification

--- CSW2/Assignment_3/src/question_11/Q11.java
+++ CSW2/Assignment_3/src/question_11/Q11.java
@@ -5,17 +5,18 @@ public class Q11
 
 	public static void main(String[] args) 
 	{
-		HashSet<Integer> hset=new HashSet<>();
+		HashMap<Integer,Integer> hmap=new HashMap<>();
 		int[] arr= {1,5,4,3,2,8,7,6,10};
 		for(int i:arr)
 		{
-			if(!hset.contains(i))
-				hset.add(i);
+			
+			if(!hmap.containsKey(i))
+				hmap.put(i, 1);
 		}
 		System.out.println("Array: "+Arrays.toString(arr));
 		for(int i=1;i<=10;i++)
 		{
-			if(!hset.contains(i))
+			if(!hmap.containsKey(i))
 			{
 				System.out.println("Smallest positive number missing: "+i);
 				break;
