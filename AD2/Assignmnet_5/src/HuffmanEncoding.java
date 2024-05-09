27ff635952c66914967ebac4b29ea7a564e1c664
Dhiraj <dpasayat14@gmail.com>
2024-05-09 09:48:23 +0530

    Huffman code

---
+++ AD2/Assignmnet_5/src/HuffmanEncoding.java
@@ -0,0 +1,94 @@
+import java.util.*;
+public class HuffmanEncoding 
+{
+	
+	class HuffmanNode implements Comparable<HuffmanNode>
+	{
+		int f;
+		char ch;
+		HuffmanNode left;
+		HuffmanNode right;
+		HuffmanNode(char ch, int freq)
+		{
+			this.ch = ch;
+			this.f = freq;
+			left=right=null;
+		}
+		@Override
+		public int compareTo(HuffmanNode o) 
+		{
+			
+			return this.f-o.f;
+		}
+		
+	}
+	
+	public void printTree(HuffmanNode root, String s)
+	{
+		if(root.left == null && root.right == null)
+		{
+			System.out.println(root.ch+" : "+s);
+			return;
+		}
+		printTree(root.left,s+"0");
+		printTree(root.right,s+"1");
+	}
+	
+	public void HuffmanTree(char[] sym, int[]freq)
+	{
+		PriorityQueue<HuffmanNode> pq = new PriorityQueue<>();
+		
+		for(int i=0; i<sym.length; i++)
+		{
+			HuffmanNode node = new HuffmanNode(sym[i],freq[i]);
+			pq.add(node);
+		}
+		HuffmanNode root=null;
+		while(pq.size()>1)
+		{
+			HuffmanNode x=pq.poll();
+			HuffmanNode y=pq.poll();
+			
+			root = new HuffmanNode('-',x.f+y.f);
+			root.left=x;
+			root.right=y;
+			
+			pq.add(root);
+		}
+		
+		printTree(pq.peek(),"");
+		
+		
+	}
+	
+	public void decode(String code, HuffmanNode root)
+	{
+		char[] cd = code.toCharArray();
+		HuffmanNode temp=root;
+		for(char ch:cd)
+		{
+			if(ch=='0')
+				temp=temp.left;
+			else
+				temp=temp.right;
+			if(temp.right==null && temp.left==null)
+			{
+				System.out.println(temp.ch);
+				temp=root;
+			}
+			
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		char[] sym = {'c','a','k','t','r'};
+		int[] freq = {75,30,105,280,110};
+		HuffmanEncoding hq=new HuffmanEncoding ();
+		hq.HuffmanTree(sym,freq);
+		
+
+	}
+
+}
+
