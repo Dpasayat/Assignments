f9edd5752a245bc29e5b2a3b25541a17953bd6ac
Dhiraj <dpasayat14@gmail.com>
2024-05-14 09:15:50 +0530

    Matrix Multiplication half done

--- CSW2/Demo/Demo/src/Threads_Demo/MatrixMultiplication.java
+++ CSW2/Demo/Demo/src/Threads_Demo/MatrixMultiplication.java
@@ -44,5 +44,27 @@ public class MatrixMultiplication
 		}
 		return result;
 	}
+	
+	public static int[][] multithreadingMultiplyMatrix(int[][] matrix1, int[][] matrix2)
+	{
+		int row1 = matrix1.length;
+		int column1 = matrix1[0].length;
+		int column2 = matrix2[0].length;
+		int[][] result = new int[row1][column1];
+
+		for (int i = 0; i < row1; i++) 
+		{
+			for (int j = 0; j < column2; j++) 
+			{
+				result[i][j] = 0;
+				for (int k = 0; k < column1; k++) 
+				{
+					result[i][j] += matrix1[i][k] * matrix2[k][j];
+				}
+			}
+		}
+		return result;
+		
+	}
 
 }
